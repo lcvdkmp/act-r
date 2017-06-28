b8b0809c45d1ba1fd07eb852765654bb0eff434f
Luca van der Kamp <lucavdk@hotmail.com>
2017-06-28 15:11:04 +0200

    More stuff

--- read.py
+++ read.py
@@ -2,7 +2,8 @@ import pyactr as actr
 
 
 class Model:
-    def __init__(self):
+    def __init__(self, gui=True):
+        self.gui = gui
         # Left-to-right reading. Start at (0, 0)
         self.environment = actr.Environment(focus_position=(0, 0))
 
@@ -20,9 +21,11 @@ class Model:
                                            typename="goal", state="start"))
 
         self.model.visualBuffer("visual", "visual_location", self.model.decmem,
-                                finst=float("inf"))
+                                # finst=float("inf"))
+                                finst=5)
 
         # Find a word on the screen
+        # TODO: maybe change to lowest
         self.model.productionstring(name="find word", string="""
             =g>
             isa goal
@@ -50,7 +53,7 @@ class Model:
             ==>
             =g>
             isa goal
-            state 'retrieving'
+            state 'encoding'
             +visual>
             isa _visual
             cmd move_attention
@@ -58,27 +61,69 @@ class Model:
             ~visual_location>
         """)
 
+        self.dummy_parse()
+
+    # encoding -> encoding_done
+    def dummy_parse(self):
         # Dummy recall.
-        self.model.productionstring(name="recalling", string="""
+        self.model.productionstring(name="waiting for word", string="""
             =g>
             isa goal
-            state 'retrieving'
+            state 'encoding'
             =visual>
             isa _visual
-            value =val
+            value "___"
             ==>
             =g>
             isa goal
-            state 'retrieval_done'
+            state 'start'
+        """)
+
+        self.model.productionstring(name="recalling", string="""
+            =g>
+            isa goal
+            state 'encoding'
+            ?visual>
+            buffer full
+            =visual>
+            isa _visual
+            value =val
+            value ~"___"
+            value ~None
+            ==>
+            =g>
+            isa goal
+            state 'encoding_done'
             +retrieval>
             isa word
             form =val
         """)
 
+        # XXX: might not be realistic.
+        # The problem is that when a visual object dissapears from the
+        # environment while it was found (and therefore being attended), the
+        # value of the visual buffer will be set to None.
+        # We have to make sure we try to find a new word when this happens
+        # to find a new word.
+        self.model.productionstring(name="recover from lost word", string="""
+            =g>
+            isa goal
+            state 'encoding'
+            ?visual>
+            state free
+            =visual>
+            isa _visual
+            value None
+            ==>
+            =g>
+            isa goal
+            state 'start'
+        """)
+
         self.model.productionstring(name="lexeme retrieved", string="""
             =g>
             isa goal
-            state 'retrieval_done'
+            state 'encoding_done'
             ?retrieval>
             buffer full
             state free
@@ -95,51 +140,95 @@ class Model:
         self.model.productionstring(name="no lexeme found", string="""
             =g>
             isa goal
-            state 'retrieval_done'
+            state 'endoding_done'
             ?retrieval>
             state error
             ==>
             =g>
             isa goal
             state 'start'
-            +manual>
-            isa _manual
-            cmd press_key
-            key 'space'
         """)
 
-    def sentence_to_env(self, s):
-        margin = (30, 10)
-        pos = list(margin)
-        spacing = (60, 12)
-        max_s = self.environment.size
-        for w in s.split(' '):
-            yield {'text': w, 'position': tuple(pos)}
-            pos[0] += spacing[0]
-            if pos[0] > max_s[0]:
-                pos[0] = margin[0]
-                pos[1] += spacing[1]
+    # encoding -> encoding_done
+    def parse(self):
+        pass
 
-    def run(self):
+    def sentence_to_env(self, s):
+        wl = s.split(' ')
+
+        return [{p: self.env_word(wl, p), p+1: self.env_dash(p+1)}
+                for p in range(len(wl))]
+
+    def sentence_to_env2(self, s):
+        wl = s.split(' ')
+        return [dict(enumerate(self.dashed_env(wl, p)))
+                for p in range(len(wl))]
+
+    TEXT_MARGIN = (30, 10)
+    TEXT_SPACING = (60, 12)
+
+    def dashed_env(self, wl, p):
+        pos = list(self.TEXT_MARGIN)
+        max_s = self.environment.size
+        for (i, w) in enumerate(wl):
+            yield {'text': w if i == p else '_' * len(w),
+                   'position': tuple(pos)}
+            pos[0] += self.TEXT_SPACING[0]
+            if pos[0] > max_s[0]:
+                pos[0] = self.TEXT_MARGIN[0]
+                pos[1] += self.TEXT_SPACING[1]
+
+    def env_pos(self, p):
+        pos = list(self.TEXT_MARGIN)
+        max_s = self.environment.size
+        x = ((p * self.TEXT_SPACING[0]) %
+             (max_s[0] - 2 * self.TEXT_MARGIN[0])) + pos[0]
+        y = int((p * self.TEXT_SPACING[0]) /
+                (max_s[0] - 2 * self.TEXT_MARGIN[0])) \
+            * self.TEXT_SPACING[1] + self.TEXT_MARGIN[1]
+        return (x, y)
+
+    def env_word(self, wl, p):
+        w = wl[p]
+        return {'text': w, 'position': self.env_pos(p)}
+
+    def env_dash(self, p):
+        return {'text': '___', 'position': self.env_pos(p)}
+
+    def sim(self):
         s = " ".join(self.lexicon)
         # The simulation requires a dictionary for some reason...
-        w = dict(enumerate(self.sentence_to_env(s)))
+        # w = dict(enumerate(self.sentence_to_env(s)))
+        # print(w)
         sim = self.model.simulation(
             realtime=True,
-            gui=True,
+            gui=self.gui,
             environment_process=self.environment.environment_process,
-            stimuli=w,
-            triggers='',
-            times=1)
+            stimuli=self.sentence_to_env(s),
+            triggers=['space'],
+            times=10000)
+        return sim
 
+    def run(self):
         # while True:
         #     sim.step()
         #     if sim.current_event.action.startswith("RETRIEVED:"):
         #         print("{}: {}".format(sim.current_event.time,
         #                               sim.current_event.action))
-        sim.run()
+        # sim.run()
+        self.sim().run()
 
 
 if __name__ == "__main__":
-    m = Model()
-    m.run()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dry-run", help="Don't run the model",
+                        action="store_true")
+    parser.add_argument("-g", "--gui", help="Run with a gui",
+                        action="store_true")
+    args = parser.parse_args()
+
+    m = Model(gui=args.gui)
+    sim = m.sim()
+    if not args.dry_run:
+        sim.run()
